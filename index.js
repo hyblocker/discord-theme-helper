78b5a78f05753aaae630223e383804e39513a779
Hyblocker <bryan.spiteri@gmail.com>
2021-09-26 17:20:20 +0200

    Revert index.js

--- index.js
+++ index.js
@@ -1,76 +1,225 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
+const { findInReactTree } = require('powercord/util');
 const { React, getModule } = require('powercord/webpack');
 
-// Track if cumcord was injected before
-let hasCummed = false;
-let settingsPatch;
+// list of elements which we whitelist giving the mouse position to
+const rippleElements = [
+    'message-2qnXI6',
+    'container-2Pjhx-',
+    'containerDefault--pIXnN',
+    'labelContainer-1BLJti',
+    'item-PXvHYJ',
+    'channel-2QD9_O',
+    'content-1x5b-n',
+    'listRow-hutiT_',
+    'resultFocused-3aIoYe',
+    'item-2J2GlB',
+    'actionButton-VzECiy',
+    'autocompleteRowVertical-q1K4ky',
+];
+
+let _this = {};
 
 module.exports = class HyblockerThemeHelper extends Plugin {
-
+    constructor(...args) {
+        super(...args);
+        this.mouseEventBind = this.mouseEventBind.bind(this)
+        _this = this;
+        _this.userCache = {};
+    }
+    
     async startPlugin() {
-        // Don't inject if cumcord was already injected
-        if (!window.cumcord) {
-            // cum on your cord :)
-            const response = await fetch("https://cdn.jsdelivr.net/gh/Cumcord/Cumcord@stable/dist/build.js");
-            const text = await response.text();
-            eval(`${text}
-            //# sourceURL=https://cdn.jsdelivr.net/gh/Cumcord/Cumcord@stable/dist/build.js`);
+        
+        document.body.addEventListener("mousemove", this.mouseEventBind("mouse"));
+        document.body.addEventListener("mousedown", this.mouseEventBind("click"));
 
-            if (window.cumcord) {
-                hasCummed = true;
+        const friendRow = await getModule(m => m.displayName === 'PeopleListItem', false).prototype;
+        _this.getPrimaryColorForAvatar = await getModule(["getPrimaryColorForAvatar"]);
+        
+        // Inject CSS into the friends row, exposing banners and accent colors
+        inject(
+            "theme-helper-friendRowPatch",
+            friendRow,
+            "render",
+            this.friendRowPatch
+        );
+    }
 
-                // Import Theme Helper
-                window.cumcord.plugins.importPlugin("https://hyblocker-discord.github.io/cumcord-plugins/ThemeHelper/dist/");
+    pluginWillUnload() {
+        
+        document.body.removeEventListener("mousemove", this.mouseEventBind("mouse"));
+        document.body.removeEventListener("mousedown", this.mouseEventBind("click"));
+        
+        powercord.api.settings.unregisterSettings(this.entityID);
+        
+        uninject("theme-helper-friendRowPatch");
+    }
 
-                // Check if cumcord was ever injected via a wrapper
-                // TODO: Some global settings API rather than PC-Settings API, probably using nests, but idk how nests works so its a TODO
-                if (this.settings.get("CUMCORD_FIRST_TIME", true)) {
+    mouseEventBind(param) {
+        return function (e) {
+            // Get the element
+            e = e || window.event;
+            let target = e.target || e.srcElement;
+            let foundTarget = false;
 
-                    // Show the modal whenever the user enters settings the first time
-                    const Settings = window.cumcord.modules.webpack.findByDisplayName("SettingsView");
-                    settingsPatch = window.cumcord.patcher.after("render", Settings.prototype, (args, res) => {
-
-                        // Check if we're in the user settings menu
-                        const sidebarItems = res.props.sidebar.props.children;
-                        const position = sidebarItems.findIndex((item) => { return item.key == "changelog" }) - 1;
-                        if (position < 0) return res;
-
-                        if (this.settings.get("CUMCORD_FIRST_TIME", true)) {
-
-                            // Show the modal informing the user about what cumcord is, so that they aren't alarmed
-                            window.cumcord.ui.modals.showConfirmationModal(
-                                {
-                                    header: "It seems that this is your first time using a Cumcord plugin!",
-                                    content: `Hey, it looks like you've installed a Cumcord plugin via ${this.manifest.name}. Cumcord is a plugin that allows developers to write plugins that work on every mod. As such, you'll notice that you now have a "Cumcord" area in your Discord settings. Don't be alarmed, this page exists to configure the plugins you use that depend on Cumcord.`,
-                                    confirmText: "OK",
-                                    cancelText: null,
-                                    type: "confirm"
-                                }
-                            ).then();
-
-                            // Don't show the modal ever again
-                            this.settings.set("CUMCORD_FIRST_TIME", false);
-                        }
-
-                        return res;
-                    });
+            for (let j = 0; j < rippleElements.length; j++)
+            {
+                if (target.classList.contains(rippleElements[j])) {
+                    foundTarget = true;
+                    break;
                 }
+            }
 
-                console.log(`Loaded ${this.manifest.name}`);
+            // Check up to 5 parents up if the element has an after
+            for (let i = 0; i < 4 && !foundTarget; i++) {
+                if (target.parentElement != null) {
+                    target = target.parentElement;
+                    for (let j = 0; j < rippleElements.length && !foundTarget; j++) {
+                        if (target.classList.contains(rippleElements[j]))
+                            foundTarget = true;
+                    }
+                }
+            }
+
+            if (foundTarget) {
+                // Get the mouse position relative to the element
+                const rect = target.getBoundingClientRect();
+                let x = e.clientX - rect.left; //x position within the element.
+                let y = e.clientY - rect.top;  //y position within the element.
+                x -= rect.width / 2;
+                y -= rect.height / 2;
+
+                // Tell the CSS
+                target.style.setProperty("--" + param + "X", x + "px");
+                target.style.setProperty("--" + param + "Y", y + "px");
             }
         }
     }
 
-    pluginWillUnload() {
-        console.log("Unloaded!");
-        if (hasCummed) {
-            // Uninject modal patch
-            if (settingsPatch) {
-                settingsPatch();
+    friendRowPatch(_, res) {
+
+        const items = res.props.children();
+        const userObj = findInReactTree(items, e=> e && e.user)?.user;
+        const userData = _this.fetchUser(userObj.id);
+
+        let accentColor = null;
+        if (userObj.accentColor)  {
+            accentColor = userObj.accentColor;
+            _this.cacheUser(userObj);
+        } else if (userData.accentColor) {
+            // fallback to accent if possible
+            accentColor = userData.accentColor;
+        }
+        else {
+            // fallback to autogen
+            _this.getPrimaryColorForAvatar.getPrimaryColorForAvatar(userObj.getAvatarURL())
+                .then(args => _this.cacheUser(userObj, { accentColorGenerated: args }));
+            
+            accentColor = userData.autoAccent;
+        }
+        
+        accentColor = _this._numberToRgba(accentColor);
+        
+        // Add the attributes
+        const modified = React.cloneElement(items.props.children, {
+            // return discord props which get lost during injection
+            'role': "listitem",
+            'data-list-item-id': `people-list___${userObj.id}`,
+            'tabindex': -1,
+
+            // inject additional props
+            'data-user-id': userObj.id,
+            'data-banner-url': userData.bannerURL,
+            'data-accent-color': accentColor,
+
+            // style
+            style: {
+                "--user-banner": userData.bannerURL ? `url("${userData.bannerURL}")` : null,
+                "--user-accent-color": accentColor,
+                ...items.props.children.props.style
             }
-            // Uninject cumcord itself
-            cumcord.uninject();
+        });
+
+        res.props.children = function() { return modified };
+
+        return res;
+    }
+
+    // Stolen from https://github.com/powercord-community/rolecolor-everywhere/blob/master/index.js#L388-L394
+    _numberToRgba (color, alpha = 1) {
+        const { r, g, b } = _this._numberToRgb(color);
+        if (alpha === 1) {
+          return `rgb(${r}, ${g}, ${b})`;
+        }
+        return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    }
+
+    _numberToRgb (color) {
+        const r = (color & 0xFF0000) >>> 16;
+        const g = (color & 0xFF00) >>> 8;
+        const b = color & 0xFF;
+        return {
+            r,
+            g,
+            b
+        };
+    }
+
+    _rgbToNumber (rgb) {
+        return (((rgb[0] << 8) + rgb[1]) << 8) + rgb[2];
+    }
+
+    cacheUser(user, props) {
+        let savedUser = null;
+        let shouldSave = false;
+        let intEncodedColor = null;
+
+        // Fill props
+        if (props?.accentColorGenerated) intEncodedColor = _this._rgbToNumber(props.accentColorGenerated);
+
+        // Fetch user, so that we update it (and not overwrite)
+        if (_this.userCache[user.id]) {
+            savedUser = _this.userCache[user.id];
+        } else {
+            savedUser = _this.settings.get(user.id, {
+                bannerURL: null,
+                accentColor: null,
+                autoAccent: null,
+            });
+        }
+
+        // Check for a diff if the user was saved
+        if (!(user.bannerURL == null && user.accentColor == null) && user.bannerURL != savedUser.bannerURL) {
+            savedUser.bannerURL = user.bannerURL;
+            shouldSave = true;
+        }
+        if (user.accentColor && user.accentColor != savedUser.accentColor) {
+            savedUser.accentColor = user.accentColor;
+            shouldSave = true;
+        }
+        if (props?.accentColorGenerated && intEncodedColor != savedUser.autoAccent) {
+            savedUser.autoAccent = intEncodedColor;
+            shouldSave = true;
+        }
+
+        if (shouldSave) {
+            _this.settings.set(user.id, savedUser);
+            _this.userCache[user.id] = savedUser;
+            console.log("[Hyblocker's Theme Helper]", `Cached user "${user.username}#${user.discriminator}"!`);
+        }
+    }
+    
+    fetchUser(userId) {
+        if (_this.userCache[userId]) {
+            return _this.userCache[userId];
+        } else {
+            _this.userCache[userId] = _this.settings.get(userId, {
+                bannerURL: null,
+                accentColor: null,
+                autoAccent: null,
+            });
+            return _this.userCache[userId];
         }
     }
 };
